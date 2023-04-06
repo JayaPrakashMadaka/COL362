a67a3e6afc7a95187f2cdf309b182d16bb09489b
Jaya Prakash <96733035+JayaPrakashMadaka@users.noreply.github.com>
2023-04-06 18:33:04 +0530

    Update 2020CS10356_2020CS10390.cpp

--- 2020CS10356_2020CS10390.cpp
+++ 2020CS10356_2020CS10390.cpp
@@ -1,13 +1,81 @@
 #include <bits/stdc++.h>
-#include <iostream>
 using namespace std;
 
-//int external_merge_sort_withstop(const char* input,const char* output,const long key_count,const int k=2,const num_merges =0);
+const int SIZE = 26;
 
+struct TrieNode
+{
+  struct TrieNode *children[SIZE];
+  int count;
+  int index;
+  bool end;
+
+  TrieNode()
+    {
+      for (int i = 0; i < SIZE; i++){
+            children[i] = NULL;
+      }
+      count = 0;
+      index = -1;
+      end = false;
+    }
+};
+
+void insert(struct TrieNode *root,string key,int index) {
+  struct TrieNode *ptr = root;
+  for(int i=0;i<key.size();i++){
+    if(!ptr->children[key[i]-'a']){
+      ptr->children[key[i]-'a'] = new TrieNode();
+    }
+    ptr = ptr->children[key[i]-'a'];
+  }
+  ptr->end = true;
+  ptr->count++;
+  ptr->index = index;
+}
+
+bool search(struct TrieNode *root, string key){
+    struct TrieNode *ptr = root;
+    for (int i = 0; i < key.size(); i++){
+        if (!ptr->children[key[i]-'a']){
+            return false;
+        }
+        ptr = ptr->children[key[i]-'a'];
+    }
+    return ptr->end;
+}
+
+void preorder(TrieNode* node, string arr[]){
+  if (node != NULL){
+    for (int i = 0; i < SIZE; i++) {
+      if(node->children[i] != NULL) {
+        if(node->children[i]->end){
+          for(int j=0;j<node->children[i]->count;j++){
+            cout << arr[node->children[i]->index] << "\n";
+          }
+        }
+        preorder(node->children[i], arr);
+      }
+    }
+  }
+}
+
+void printSorted(string arr[], int n)
+{
+    TrieNode* root = new TrieNode();
+    for (int i = 0; i < n; i++)
+        insert(root, arr[i], i);
+    preorder(root, arr);
+}
+
+
+int external_merge_sort_withstop(const char* input,const char* output,const long key_count,const int k=2,const int num_merges =0);
 
 int main(){
 
-  cout<<"yes working"<<"\n";
+  string arr[] = { "abc", "xy", "bcd" ,"abc","ahvs"};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  printSorted(arr, n);
 
   return 0;
 }
