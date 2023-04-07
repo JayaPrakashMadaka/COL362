1291741f98a5ba7f55372134316f72528035961e
JayaPrakashMadaka <jayaprakashmadaka20@gmail.com>
2023-04-07 20:50:04 +0530

    Create generate.cpp

---
+++ generate.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+  const int N = 100;
+  const int ASCII_MIN = 33;
+  const int ASCII_MAX = 126;
+
+  int n = 1000;
+  ofstream MyWriteFile("input.txt");
+
+  for(int i=0;i<n;i++){
+    string s;
+    for (int i = 0; i < N; i++) {
+      int c = rand() % (ASCII_MAX - ASCII_MIN + 1) + ASCII_MIN;
+      s.push_back((char) c);
+    }
+    MyWriteFile<<s+"\n";
+  }
+
+  MyWriteFile.close();
+
+  return 0;
+}
