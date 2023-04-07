2eab2e88197976a6be884f8b91776d6f1f73c14c
JayaPrakashMadaka <jayaprakashmadaka20@gmail.com>
2023-04-07 20:49:57 +0530

    Update 2020CS10356_2020CS10390.cpp

--- 2020CS10356_2020CS10390.cpp
+++ 2020CS10356_2020CS10390.cpp
@@ -87,23 +87,6 @@ int external_merge_sort_withstop(const char* input,const char* output,const long
 
 int main(){
 
-  const int N = 100;
-  const int ASCII_MIN = 33;
-  const int ASCII_MAX = 126;
-
-  int n = 1000;
-  ofstream MyWriteFile("input.txt");
-
-  for(int i=0;i<n;i++){
-    string s;
-    for (int i = 0; i < N; i++) {
-      int c = rand() % (ASCII_MAX - ASCII_MIN + 1) + ASCII_MIN;
-      s.push_back((char) c);
-    }
-    MyWriteFile<<s+"\n";
-  }
-
-  MyWriteFile.close();
 
   external_merge_sort_withstop("input.txt","output.txt",n,2,0);
 
