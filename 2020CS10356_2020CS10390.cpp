43bcb604cbbe010296abad41f892ad1535232b13
JayaPrakashMadaka <jayaprakashmadaka20@gmail.com>
2023-04-11 02:33:27 +0530

    Update 2020CS10356_2020CS10390.cpp

--- 2020CS10356_2020CS10390.cpp
+++ 2020CS10356_2020CS10390.cpp
@@ -2,18 +2,23 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <tuple>
 using namespace std;
 
 const int SIZE = 128;
 
-const long BUFFER_SIZE = 10*1024*1024;
+const long BUFFER_SIZE = 1*1024*1024;
+
+const long READ_BUFFER_SIZE = 1*1024*1024;
+
+const long WRITE_BUFFER_SIZE = 512*1024;
 
 struct TrieNode
 {
   struct TrieNode *children[SIZE];
   int count;
   bool end;
-
+  int index;
   TrieNode()
     {
       for (int i = 0; i < SIZE; i++){
@@ -21,10 +26,11 @@ struct TrieNode
       }
       count = 0;
       end = false;
+      index = -1;
     }
 };
 
-void insert(struct TrieNode *root,string key) {
+void insert(struct TrieNode *root,string key,int index) {
   struct TrieNode *ptr = root;
   for(int i=0;i<key.size();i++){
     if(!ptr->children[key[i]]){
@@ -34,6 +40,7 @@ void insert(struct TrieNode *root,string key) {
   }
   ptr->end = true;
   ptr->count++;
+  ptr->index = index;
 }
 
 bool search(struct TrieNode *root, string key){
@@ -45,6 +52,58 @@ bool search(struct TrieNode *root, string key){
         ptr = ptr->children[key[i]];
     }
     return ptr->end;
+}
+
+bool isEmpty(TrieNode* root)
+{
+    for (int i = 0; i < SIZE; i++)
+        if (root->children[i])
+            return false;
+    return true;
+}
+
+string getmin(TrieNode* root){
+  if(!root){
+    return "";
+  }
+  for(int i=0;i<SIZE;i++){
+    if(root->children[i]){
+      if(root->children[i]->end){
+        return string(1,(char)i);
+      }
+      return string(1,(char)i)+getmin(root->children[i]);
+    }
+  }
+  return "";
+}
+
+tuple<TrieNode*,int> remove(TrieNode* root, string key, int depth = 0)
+{
+    if (!root){
+        TrieNode* val = NULL;
+        return make_tuple(val,-1);
+    }
+    if (depth == key.size()) {
+        int index;
+        if (root->end)
+            root->end = false;
+            index = root->index;
+            root->index = -1;
+        if (isEmpty(root)) {
+            delete (root);
+            root = NULL;
+        }
+        return make_tuple(root,index);
+    }
+    int index = key[depth];
+    tuple<TrieNode*,int> t = remove(root->children[index], key, depth + 1);
+    root->children[index] = get<0>(t);
+    int val = get<1>(t);
+    if (isEmpty(root) && root->end == false) {
+        delete (root);
+        root = NULL;
+    }
+    return make_tuple(root,val);
 }
 
 void preorder(TrieNode* node, string arr ,ofstream &v)
@@ -69,12 +128,79 @@ void printSorted(const char* input,const char* output ,int begin, int n)
     string text;
     TrieNode* root = new TrieNode();
     for (int i = 0; i < n; i++){
-      if(getline (infile, text)) insert(root, text);
+      if(getline (infile, text)) insert(root,text,-1);
     }
     infile.close();
     ofstream outfile(output);
     preorder(root, "" ,outfile);
     outfile.close();
+}
+
+int check_files(vector<int> &v){
+  for(int i=0;i<v.size();i++){
+    if(v[i]==0) return 1;
+  }
+  return 0;
+}
+
+void run(int file_count,int prev_level,int count){
+  vector<ifstream*> infiles;
+  for(int i=0;i<file_count;i++){
+    ifstream x("../A3_data/temp."+to_string(prev_level)+"."+to_string(i));
+    infiles.push_back(&x);
+  }
+  ofstream outfile("../A3_data/temp."+to_string(prev_level+1)+"."+to_string(count));
+  vector<int> check(file_count,0);
+  while(check_files(check)){
+    long in_memory = 0;
+    TrieNode* root = new TrieNode();
+    int i = 0;
+    while(in_memory < READ_BUFFER_SIZE && check_files(check)){
+      long per_in_memory = 0;
+      while(per_in_memory < READ_BUFFER_SIZE/file_count && !check[i] && in_memory < READ_BUFFER_SIZE){
+        string text;
+        if(getline(*infiles[i],text)){
+          insert(root,text,i);
+          i=(i+1)%file_count;
+          per_in_memory += text.size();
+          in_memory += text.size();
+        }
+        else{
+          check[i]=1;
+          i=(i+1)%file_count;
+          break;
+        }
+      }
+    }
+    while(root){
+      long out_memory = 0;
+      vector<string> write_out;
+      while (out_memory < WRITE_BUFFER_SIZE && check_files(check) && root) {
+        string val = getmin(root);
+        write_out.push_back(val);
+        out_memory += val.size();
+        int index = get<1>(remove(root,val,0));
+        string text;
+        if(getline(*infiles[index],text)){
+          insert(root,text,index);
+        }
+        else{
+          check[index]=1;
+        }
+      }
+      for(int i=0;i<write_out.size();i++){
+        outfile<<write_out[i]<<"\n";
+      }
+      //delete &write_out;
+    }
+  }
+
+  outfile.close();
+
+  for(int i=0;i<file_count;i++){
+    infiles[i]->close();
+  }
+
 }
 
 
@@ -84,34 +210,35 @@ int external_merge_sort_withstop(const char* input,const char* output,const long
 
   long words = 0;
 
-  int run = 0;
+  int number_runs = 0;
 
-  while(words < key_count){
+  /*while(words < key_count){
     long memory = 0;
     long count = 0;
     TrieNode* root = new TrieNode();
     while(memory < BUFFER_SIZE && words < key_count){
       string text;
       if(getline(infile,text)){
-        insert(root,text);
+        insert(root,text,-1);
         memory += text.size();
         words++;
       }
     }
-    ofstream outfile("../A3_data/temp.0."+to_string(run));
+    ofstream outfile("../A3_data/temp.0."+to_string(number_runs));
     preorder(root,"",outfile);
     outfile.close();
     delete root;
-    run++;
-  }
+    number_runs++;
+  }*/
 
+  run(k,0,0);
 
   return num_merges;
 }
 
 int main(){
 
-  long n= 600000;
+  long n= 50000;
 
   external_merge_sort_withstop("../A3_data/input.txt","../A3_data/output.txt",n,2,0);
 
